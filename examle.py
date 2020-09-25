3f91d4ab192e201b66ab51e4404012508622e965
BollaRajesh <45807971+BollaRajesh@users.noreply.github.com>
2020-09-25 11:17:36 +0530

    Add files via upload

---
+++ examle.py
@@ -0,0 +1,18 @@
+##
+##a=int(input())
+##b=int(input())
+##
+##
+##import mod
+##print(mod.add(a,b))
+##
+##
+##from mod import add
+##print(add(a,b))
+##
+##
+
+
+import seconddegree
+n=int(input('enter a number:'))
+print(seconddegree.prime(n))
